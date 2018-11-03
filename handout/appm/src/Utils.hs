04802d3725206e805ed19c2d0fb98862b527c4d6
Silvan Adrian <hallo@silvanadrian.ch>
2018-11-03 11:02:35 +0100

    keep going

--- handout/appm/src/Utils.hs
+++ handout/appm/src/Utils.hs
@@ -4,9 +4,18 @@ module Utils where
 -- should be placed here.
 
 import Defs
+import Data.Bool
 
 instance Ord Version where
-  (<=) = V[]    -- or define 'compare' instead
+  (<=) (V[]) _ = False
+  (<=) (V(_:_)) (V []) = True
+  (<=) (V[VN v1int v1str])  (V[VN v2int v2str]) =
+    if checkVersion v1int v2int v1str v2str then True else False
+  (<=) (V(VN v1int v1str:xs))  (V(VN v2int v2str:ys)) = V(xs) <= V(ys)
+
+checkVersion :: Int -> Int -> String -> String -> Bool
+checkVersion a b c d = a <= b && (c <= d || length(c) <= length(d))
+  -- or define 'compare' instead
 
 merge :: Constrs -> Constrs -> Maybe Constrs
 merge = undefined
--- handout/appm/tests/BB/Main.hs
+++ handout/appm/tests/BB/Main.hs
@@ -9,8 +9,28 @@ import Solver (install)
 import Test.Tasty
 import Test.Tasty.HUnit
 
+
+tests = testGroup "Unit Tests" [
+    utilities,
+    predefined
+    ]
+
+utilities = testGroup "Utilities tests" [
+        testCase "Version 1 <= 2" $ V [VN 1 ""] <= V [VN 2 ""] @?= True,
+        testCase "Version 2 <= 1" $ V [VN 2 ""] <= V [VN 1 ""] @?= False,
+        testCase "Version 1a <= Verion1z" $ V [VN 1 "a"] <= V [VN 1 "z"] @?= True,
+        testCase "Version 1.1 <= 1.2" $ V [VN 1 "", VN 1 ""] <= V [VN 1 "", VN 2 ""] @?= True,
+        testCase "Version 1.2 <= 1.1" $ V [VN 1 "", VN 2 ""] <= V [VN 1 "", VN 1 ""] @?= False,
+        testCase "Version 1.1a <= 1.1b" $ V [VN 1 "", VN 1 "a"] <= V [VN 1 "", VN 1 "b"] @?= True,
+        testCase "Version 4.0.1 <= 04.00.001" $ V [VN 4 "", VN 0 "", VN 1 ""] <= V [VN 04 "", VN 00 "", VN 001 ""] @?= True,
+        testCase "Version 4.0.1.3 <= 4.1.2" $ V [VN 4 "", VN 0 "", VN 1 "", VN 3 ""] <= V [VN 4 "", VN 1 "", VN 2 ""] @?= True,
+        testCase "802.11 <= 802.11n" $ V [VN 8 "", VN 0 "", VN 2 "", VN 1 "", VN 1 ""] <= V [VN 8 "", VN 0 "", VN 2 "", VN 1 "", VN 1 "n"] @?= True,
+        testCase "802.11n <= 802.11ax" $ V [VN 8 "", VN 0 "", VN 2 "", VN 1 "", VN 1 "n"] <= V [VN 8 "", VN 0 "", VN 2 "", VN 1 "", VN 1 "ax"] @?= True,
+        testCase "802.11ax <= 802.11bb" $ V [VN 8 "", VN 0 "", VN 2 "", VN 1 "", VN 1 "ax"] <= V [VN 8 "", VN 0 "", VN 2 "", VN 1 "", VN 1 "bb"] @?= True
+    ]
+
 -- just a sample; feel free to replace with your own structure
-tests = testGroup "Unit tests"
+predefined = testGroup "Unit tests"
   [testGroup "Parser tests"
      [testCase "tiny" $
         parseDatabase "package {name foo}" @?= Right db],
