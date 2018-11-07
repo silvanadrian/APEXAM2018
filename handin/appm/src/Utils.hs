a54d62eea40f33946f9555302b7e1b7ad4436f76
Silvan Adrian <hallo@silvanadrian.ch>
2018-11-07 18:40:45 +0100

    util tests

--- handin/appm/src/Utils.hs
+++ handin/appm/src/Utils.hs
@@ -19,18 +19,41 @@ merge :: Constrs -> Constrs -> Maybe Constrs
 merge [] [] = Just []
 merge c1 [] = Just c1
 merge [] c2 = Just c2
--- merge ((pname1,(bool1, miv1, mxv1)):xs) ((pname2,(bool2, miv2,mxv2)):ys) =
---                                                            if miv1 <= (miv2 <= mxv1)  then
---                                                                 if miv1 <= (mxv1 <= mxv2) then
---                                                                     Just [(pname2, (bool1,miv1, mxv2))]
---                                                                 else
---                                                                     Nothing
---                                                            else
---                                                                 Nothing
-merge (pkg:c1) (c2) = case pkgInC2 pkg c2 [] of
+merge (const:c1) (c2) = case constInC2 const c2 [] of
                             Just x -> merge c1 x
                             Nothing -> Nothing
 
+-- Check if Constraint from c1 is in the Constraint list C2
+constInC2 :: (PName, PConstr) -> Constrs -> Constrs -> Maybe Constrs
+constInC2 _ [] x = Just x
+constInC2 const (c2const:c2tail) x =
+                case fst const == fst c2const of
+                    True -> case mergeConst (snd const) (snd c2const) of
+                                Nothing -> Nothing
+                                Just mconst -> Just (x ++ [(fst const, mconst)] ++ c2tail)
+                    False -> constInC2 const c2tail (x ++ [c2const])
 
-pkgInC2 :: (PName, PConstr) -> Constrs -> Constrs -> Maybe Constrs
-pkgInC2 _ [] x = Just x
+-- Compare the 2 Constraints with
+mergeConst :: PConstr -> PConstr -> Maybe PConstr
+mergeConst (b1,c1v1,c1v2) (b2,c2v1,c2v2)
+        | c1v2 <= c2v1 = Nothing
+        | c2v2 <= c1v1 = Nothing
+        | b1 == True && b2 == True = Just (b1, (largest c1v1 c2v1), (smallest c1v2 c2v2))
+        | b1 == False && b2 == False = Just (b1, (largest c1v1 c2v1), (smallest c1v2 c2v2))
+        | b1 == True && b2 == False = Just (b1, (largest c1v1 c2v1), (smallest c1v2 c2v2))
+        | b1 == False && b2 == True = Just (b2, (largest c1v1 c2v1), (smallest c1v2 c2v2))
+mergeConst _ _ = Nothing
+
+-- Return the smaller of 2 Versions
+smallest :: Version -> Version -> Version
+smallest v1 v2 =
+    case v1 <= v2 of
+        True -> v1
+        False -> v2
+
+-- Returns the bigger of 2 Versions
+largest :: Version -> Version -> Version
+largest v1 v2 =
+    case v1 >= v2 of
+        True -> v1
+        False -> v2
--- handin/appm/tests/BB/Main.hs
+++ handin/appm/tests/BB/Main.hs
@@ -13,30 +13,61 @@ import Test.Tasty.HUnit
 
 tests = testGroup "Unit Tests"
     [
-        utilities,
-        predefined
+        utilities
+        --predefined
     ]
 
 utilities = testGroup "Utilities tests"
     [
         -- Versions
         testCase "Version 1 <= 1" $ V [VN 1 ""] <= V [VN 1 ""] @?= True,
-        testCase "Version 1 <= 2" $ V [VN 1 ""] <= V [VN 2 ""] @?= True,
-        testCase "Version 2 <= 1" $ V [VN 2 ""] <= V [VN 1 ""] @?= False,
-        testCase "Version 1a <= Verion1z" $ V [VN 1 "a"] <= V [VN 1 "z"] @?= True,
-        testCase "Version 1.1 <= 1.2" $ V [VN 1 "", VN 1 ""] <= V [VN 1 "", VN 2 ""] @?= True,
-        testCase "Version 1.2 <= 1.1" $ V [VN 1 "", VN 2 ""] <= V [VN 1 "", VN 1 ""] @?= False,
-        testCase "Version 1.1a <= 1.1b" $ V [VN 1 "", VN 1 "a"] <= V [VN 1 "", VN 1 "b"] @?= True,
-        testCase "Version 4.0.1 <= 04.00.001" $ V [VN 4 "", VN 0 "", VN 1 ""] <= V [VN 04 "", VN 00 "", VN 001 ""] @?= True,
-        testCase "Version 4.0.1.3 <= 4.1.2" $ V [VN 4 "", VN 0 "", VN 1 "", VN 3 ""] <= V [VN 4 "", VN 1 "", VN 2 ""] @?= True,
+        testCase "Version 1 <= 2" $
+            V [VN 1 ""] <= V [VN 2 ""] @?= True,
+        testCase "Version 2 <= 1" $
+            V [VN 2 ""] <= V [VN 1 ""] @?= False,
+        testCase "Version 1a <= Verion1z" $
+            V [VN 1 "a"] <= V [VN 1 "z"] @?= True,
+        testCase "Version 1.1 <= 1.2" $
+            V [VN 1 "", VN 1 ""] <= V [VN 1 "", VN 2 ""] @?= True,
+        testCase "Version 1.2 <= 1.1" $
+            V [VN 1 "", VN 2 ""] <= V [VN 1 "", VN 1 ""] @?= False,
+        testCase "Version 1.1a <= 1.1b" $
+            V [VN 1 "", VN 1 "a"] <= V [VN 1 "", VN 1 "b"] @?= True,
+        testCase "Version 4.0.1 <= 04.00.001" $
+            V [VN 4 "", VN 0 "", VN 1 ""] <= V [VN 04 "", VN 00 "", VN 001 ""] @?= True,
+        testCase "Version 4.0.1.3 <= 4.1.2" $
+            V [VN 4 "", VN 0 "", VN 1 "", VN 3 ""] <= V [VN 4 "", VN 1 "", VN 2 ""] @?= True,
         testCase "802.11 <= 802.11n" $ V [VN 802 "", VN 11 ""] <= V [VN 802 "", VN 11 "n"] @?= True,
         testCase "802.11n <= 802.11ax" $ V [VN 802 "", VN 11 "n"] <= V [VN 802 "", VN 11 "ax"] @?= True,
         testCase "802.11ax <= 802.11bb" $ V [VN 802 "", VN 11 "ax"] <= V [VN 802 "", VN 11 "bb"] @?= True,
-        -- Merge Constraints -> not really perfect yet no check if constraint makes sense or not
+        -- Merge Constraints
         testCase "Merge 2 Empty Lists" $ merge [] [] @?= Just [],
-        testCase "Merge non empty and empty List" $ merge [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))] [] @?= Just [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))],
-        testCase "Merge 2 non empty Lists" $ merge [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))] [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))] @?=
-        Just [(P "Test",(False,V [VN 0 ""],V [VN 1 ""])),(P "Test",(False,V [VN 0 ""],V [VN 1 ""]))]
+        testCase "Merge non empty and empty List" $ merge
+            [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))] [] @?=
+            Just [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))],
+        testCase "Merge 2 non empty Lists" $ merge
+            [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))] [(P "Test", (False, V [VN 0 ""] , V [VN 1 ""] ))] @?=
+            Just [(P "Test",(False,V [VN 0 ""],V [VN 1 ""]))],
+        testCase "Merge True and False" $ merge
+            [(P "Test", (True, V [VN 2 ""] , V [VN 8 ""] ))] [(P "Test", (False, V [VN 4 ""] , V [VN 6 ""] ))] @?=
+            Just [(P "Test",(True,V [VN 4 ""],V [VN 6 ""]))],
+        testCase "Merge True and False 2nd example" $ merge
+            [(P "Test", (True, V [VN 4 ""] , V [VN 6 ""] ))] [(P "Test", (False, V [VN 3 ""] , V [VN 8 ""] ))] @?=
+            Just [(P "Test",(True,V [VN 4 ""],V [VN 6 ""]))],
+        testCase "Merge False and False" $ merge
+            [(P "Test", (False, V [VN 4 ""] , V [VN 6 ""] ))] [(P "Test", (False, V [VN 3 ""] , V [VN 8 ""] ))] @?=
+            Just [(P "Test",(False,V [VN 4 ""],V [VN 6 ""]))],
+        testCase "Merge False and True" $ merge
+            [(P "Test", (False, V [VN 4 ""] , V [VN 6 ""] ))] [(P "Test", (True, V [VN 3 ""] , V [VN 8 ""] ))] @?=
+            Just [(P "Test",(True,V [VN 4 ""],V [VN 6 ""]))],
+        testCase "Merge Many Constrints example" $ merge
+            [(P "Test", (True, V [VN 4 ""] , V [VN 6 ""] )),  (P "Test2", (False, V [VN 3 "a"], V [VN 9 ""])),
+             (P "Test3", (False, V [VN 1 ""], V [VN 10 ""]))]
+            [(P "Test", (False, V [VN 3 ""] , V [VN 8 ""] )), (P "Test2", (False, V [VN 3 "z"], V [VN 7 ""]))] @?=
+            Just [(P "Test",(True,V [VN 4 ""],V [VN 6 ""])),(P "Test2",(False,V [VN 3 "z"],V [VN 7 ""]))],
+        testCase "Merge same Version" $ merge
+                    [(P "Test", (False, V [VN 1 ""] , V [VN 1 ""] ))] [] @?=
+                    Just [(P "Test", (False, V [VN 1 ""] , V [VN 1 ""] ))]
     ]
 
 
